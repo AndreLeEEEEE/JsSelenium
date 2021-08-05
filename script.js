4f2f6c56708ba2d4d7d506f9c84092f5ff3086b3
Andre Le <Andre.Le@wanco.local>
2021-08-05 14:09:36 -0600

    Set up basic selenium test

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+import { By, Key, Builder } from "selenium-webdriver";
+
 document.getElementById("Start").addEventListener("click", function() {
-    alert("What");
+    example();
 });
+
+async function example() {
+    let searchString = "Automation testing with Selenium and JavaScript";
+
+    // Wait for the browser to build and launch properly
+    let driver = await new Builder().forBrowser("chrome").build();
+
+    // To fetch https://www.google.com/ from the browser
+    await driver.get("https://www.google.com/");
+
+    // To send a search query by passing the value in searchString
+    await driver.findElement(By.name("q")).sendKeys(searchString,Key.RETURN);
+
+    // Verify the page title and print it
+    let title = await driver.getTitle();
+    console.log(`Title is ${title}`);
+
+    // It's always a safe practice to quit the browser after execution
+    await driver.quit();
+}
