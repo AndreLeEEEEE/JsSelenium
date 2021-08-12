dabe7dd2699b08be5327fa9646739b2edead38d7
Andre Le <Andre.Le@wanco.local>
2021-08-12 10:44:37 -0600

    Object inheritance testing

--- Dummy.js
+++ Dummy.js
@@ -1,3 +1,20 @@
-export function SayName() {
-    alert("AAAAAAAAAAAAHHHHHHHHHHHH");
-}
+let proto = {
+    hello () {
+        return `Hello, my name is ${ this.name}`;
+    }
+};
+
+let greeter = (name) => Object.assign(Object.create(proto), {
+    name,
+    state: ["false", "true"]
+});
+
+let guy = greeter("guy");
+let gal = greeter("gal");
+
+guy.state[0] = "one";
+let m = guy.state;
+let n = gal.state;
+
+console.log(m);
+console.log(n);
