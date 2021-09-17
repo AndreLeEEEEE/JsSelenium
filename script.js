d4c0918e53b97f7dff6f4ea2bb3cc82b291e9301
Andre Le <Andre.Le@wanco.local>
2021-09-17 11:49:40 -0600

    Window switching and some menu clicking

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
 const {Builder, By, Key, util, Actions} = require("selenium-webdriver");
+// Gets real finicky with promises, which is unfortunate because selenium is 95% promises
 const ks = require("node-key-sender");
-
-async function clickMenuItem(driver, itemName) {
-    let nodes = await driver.findElements(By.className("MenuNodeTitle"));
-    for (let node of nodes) {
-        let text = await node.getText();
-        if (text === itemName) {
-            return node;
-        }
-    }
-}
-
-function failureCallback() {
-    console.log("It failed");
-}
 
 async function example() {
     // Wait for the browser to build and launch properly
-    let driver = await new Builder().forBrowser("chrome").build();
+    let driver = new Builder().forBrowser("chrome").build();
 
     // To fetch https://www.google.com/ from the browser
     await driver.get("https://www.plexonline.com/modules/systemadministration/login/index.aspx");
@@ -28,12 +15,38 @@ async function example() {
     await driver.findElement(By.name("txtCompanyCode")).sendKeys("wanco", Key.RETURN);
 
     let windows = await driver.getAllWindowHandles();
-    driver.switchTo().window(windows[1]);
-
-    let nodes = await driver.findElements(By.className("MenuNodeTitle"));
-    let node = nodes.then()
-
-    // driver.quit();
+    // Apparently switchTo() doesn't return a value even though the docs
+    // say it returns a targetLocator. Thus, it can't be used in
+    // assignments or return statements.
+    await driver.switchTo().window(windows[1]);
+    return driver;
 }
 
-example();
+async function getMenuNodes(driver) {
+    return driver.findElements(By.className("MenuNodeTitle"));
+}
+
+async function getElementText(webElement) {
+    return webElement.getText();
+}
+
+async function clickOnElement(webElement) {
+    webElement.click();
+}
+
+async function KeepGoing(driver, targetNode) {
+    let nodes = await getMenuNodes(driver);
+    for (let node of nodes) {
+        let text = await getElementText(node);
+        if (text === targetNode) {
+            await clickOnElement(node);
+        }
+    }
+    return driver;
+}
+
+example()
+    .then(driver => KeepGoing(driver, "Office Menu"))
+    .then(driver => KeepGoing(driver, "Production Menu"))
+    .then(driver => KeepGoing(driver, "Production Setup Menu"))
+    .then(driver => KeepGoing(driver, "Production History"))
